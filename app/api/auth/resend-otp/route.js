1b63e3a0907002b97512869aa9ffc6c0621a9bb9
sonu <sonukumar20010520@gmail.com>
2025-09-30 11:56:11 +0530

    second commit

--- app/api/auth/resend-otp/route.js
+++ app/api/auth/resend-otp/route.js
@@ -1,6 +1,8 @@
 import { otpEmail } from "@/email/otpVerification";
 import { connectDb } from "@/lib/databaseConnection";
 import { catchError, generateOTP, response } from "@/lib/helperFunction";
+import { sendMail } from "@/lib/sendMail";
+import { zSchema } from "@/lib/zodSchema";
 import OTPMODEL from "@/models/otpModel";
 import UserModel from "@/models/userModel";
 
@@ -29,14 +31,14 @@ export async function POST(request){
         })
         await newOtpData.save();
 
-        const otpSendStatus=await  await sendMail('Your verification code', email,otpEmail(otp))
+        const otpSendStatus=await sendMail('Your verification code', email,otpEmail(otp))
 
-        if(!otpSendStatus){
+        if(!otpSendStatus.success){
             return response(false,400,'failed to resend otp')
         }
         return response(true,200,'OTP sent successfully')
     }
     catch(error){
-        catchError(error)
+        return catchError(error)
     }
 }
